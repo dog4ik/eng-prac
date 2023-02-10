c0d7c4ea10c138ded90904cffe7c10c6a3cbf83b
Vadim <angryvadik@gmail.com>
2023-02-10 19:35:51 +0300

    remove queryClient

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,17 @@
 import "../globals.css";
 import type { AppProps } from "next/app";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Layout from "../components/Layout";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { trpc } from "../utils/trpc";
 import NotificationCtxProvider from "../components/context/NotificationCtx";
-const queryClient = new QueryClient({
-  defaultOptions: { queries: { retry: false, refetchOnWindowFocus: false } },
-});
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <NotificationCtxProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-        <ReactQueryDevtools initialIsOpen={false} />
-      </NotificationCtxProvider>
-    </QueryClientProvider>
+    <NotificationCtxProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </NotificationCtxProvider>
   );
 }
 
