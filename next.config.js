c11fe5fb81062b1314be6d5cd7fb1c30744b5ca3
Vadim <angryvadik@gmail.com>
2023-03-10 10:21:50 +0100

    localhost

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["images.unsplash.com", "image.tmdb.org"],
+    domains: ["images.unsplash.com", "image.tmdb.org", "localhost"],
   },
 };
 
