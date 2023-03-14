73cb73b253869ea5e22dba6523b935b0f5ae6688
Vadim <angryvadik@gmail.com>
2023-03-14 19:02:34 +0100

    Return back

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,13 +1,19 @@
 import React from "react";
-import Link from "next/link";
+import { useRouter } from "next/router";
 
 const NotFound = () => {
+  const router = useRouter();
   return (
     <div className="absolute top-1/2 left-1/2 flex -translate-y-1/2 -translate-x-1/2 flex-col items-center justify-center gap-10 md:flex-row">
       <h1 className="text-2xl">Page not found</h1>
-      <Link href={"/"} className="rounded-xl bg-green-400 p-2 text-white">
+      <button
+        onClick={() => {
+          router.back();
+        }}
+        className="rounded-xl bg-green-400 p-2 text-white"
+      >
         Return
-      </Link>
+      </button>
     </div>
   );
 };
